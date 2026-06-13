df1e41e453652d7eff60725655bedb3fef318972
magaraj <magarajcb@gmail.com>
2026-06-13 21:17:09 +0530

    mongo db connected

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const http=require('http')
 const app=require("./app")
+const dotenv=require("dotenv")
+dotenv.config();
+const connnectDB=require("./config/db")
+connnectDB();
+
 const server=http.createServer(app)
-server.listen(3001,()=>{
-    console.log("Server running at 3001")
+server.listen(process.env.PORT,()=>{
+    console.log(`Server running at 3001 ${process.env.PORT}`)
 })
