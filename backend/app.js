ffd3bc556c8a837c4ad37872101542e4afe953a0
magaraj <magarajcb@gmail.com>
2026-06-15 20:29:35 +0530

    regisered succesfully

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express=require("express")
+const authRoutes=require("./routes/authRoutes")
 const app=express();
+app.use(express.json())
 app.get("/",(req,res)=>{
     res.send("Live chat app backend running")
 })
+app.use("/api/auth",authRoutes)
 module.exports=app
